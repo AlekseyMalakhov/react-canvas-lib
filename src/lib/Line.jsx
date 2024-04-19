ab48adeb10b91a2682ccc94680c0c69b2d34a20c
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-04-19 15:04:16 +0300

    show line

--- src/lib/Line.jsx
+++ src/lib/Line.jsx
@@ -4,7 +4,14 @@ import { CanvasContext } from "./Canvas";
 export default function Line({ start, end }) {
   const canvas = useContext(CanvasContext);
 
-  console.log(canvas);
+  if (canvas?.getContext) {
+    const ctx = canvas.getContext("2d");
+
+    ctx.beginPath();
+    ctx.moveTo(start[0], start[1]);
+    ctx.lineTo(end[0], end[1]);
+    ctx.stroke();
+  }
 
   return null;
 }

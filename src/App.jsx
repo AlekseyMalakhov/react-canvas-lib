a55734c517184516b66c33ea047bec33913055d4
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-04-19 15:13:04 +0300

    add rect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Canvas from "./lib/Canvas";
 import Line from "./lib/Line";
+import Rectangle from "./lib/Rectangle";
 
 function App() {
   return (
@@ -7,6 +8,11 @@ function App() {
       <div>Canvas library</div>
       <Canvas width={300} height={150}>
         <Line start={[20, 30]} end={[80, 150]} />
+        <Rectangle start={[70, 60]} end={[170, 100]} />
+      </Canvas>
+      <Canvas width={300} height={150}>
+        <Line start={[30, 70]} end={[150, 30]} />
+        <Rectangle start={[100, 60]} end={[170, 140]} />
       </Canvas>
     </main>
   );
---
+++ src/lib/Rectangle.jsx
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import { CanvasContext } from "./Canvas";
+
+export default function Rectangle({ start, end }) {
+  const canvas = useContext(CanvasContext);
+
+  if (canvas?.getContext) {
+    const ctx = canvas.getContext("2d");
+
+    const width = end[0] - start[0];
+    const height = end[1] - start[1];
+
+    ctx.fillRect(start[0], start[1], width, height);
+  }
+
+  return null;
+}
